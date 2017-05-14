272647a91a6dc39926811276615bc6a49e747aa9
Lihui Shi <shilihui@uw.edu>
2017-05-13 20:16:18 -0700

    Create format.py

---
+++ format.py
@@ -0,0 +1,8 @@
+# This is new in Python 3, and in Python 2 it is different
+a = 'romeo'
+b = 'juliet'
+c = 2
+output1 = '{} and {} love each other'.format(a, b)
+output2 = '{} and {}, those {} persons love each other'.format(a, b, c)
+print(output1)
+print(output2)
