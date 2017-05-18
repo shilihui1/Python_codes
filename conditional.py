f2b6b9be73ee3f812092aae2054161b367213fe4
Lihui Shi <shilihui@uw.edu>
2017-05-17 20:17:51 -0700

    Update conditional.py

--- conditional.py
+++ conditional.py
@@ -1,3 +1,3 @@
-# conditional expression in Python
+# two different conditional expression in Python: conditional execution and conditioal value
 a, b = 1, 0
 print("b ({}) is larger".format(b) if a < b else "a ({}) is larger".format(a))
