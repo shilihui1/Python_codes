8d5b00050f6ca552bdd9579aee18db5ec5099f2f
Lihui Shi <shilihui@uw.edu>
2017-05-31 19:17:33 -0700

    Create nparray_forloop.py

---
+++ nparray_forloop.py
@@ -0,0 +1,12 @@
+import numpy as np
+a = np.array([1, 2, 3, 4])
+b = np.array(['a', 'b', 'c', 'd'])
+c = np.array([a, b])
+print(c)
+print(type(c))
+
+for i in c:
+    print(i)
+
+for i in np.nditer(c):
+    print(i)
