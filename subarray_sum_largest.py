ffb59375b6d91713f4c81eff3046e951b752bf48
Lihui Shi <shilihui@uw.edu>
2017-06-03 10:59:03 -0700

    Create subarray_sum_largest.py

---
+++ subarray_sum_largest.py
@@ -0,0 +1,15 @@
+class solution:
+    def subarray_sum_largest(x):
+        n = len(x)
+        temp_largest = sum(x)
+        # from length 1 to length n
+        for i in range(1, n):
+            # move the starting point for length i
+            for j in range(n - i + 1):
+                a = sum(x[j:j + i - 1])
+                if a > temp_largest:
+                    temp_largest = a
+                else:
+                    pass
+        return temp_largest
+print(solution.subarray_sum_largest([-1, 4, -3, 4, -5]))
