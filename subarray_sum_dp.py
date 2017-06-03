1438cf434cac8accb8e52523546c62e081c33800
Lihui Shi <shilihui@uw.edu>
2017-06-03 11:52:45 -0700

    Create subarray_sum_dp.py

---
+++ subarray_sum_dp.py
@@ -0,0 +1,16 @@
+''' 
+define the largest subarray sum of a_0, ..., a_i is s(i),
+then we have: s(i+1) = max(s(i), a_(i+1))
+'''
+class solution:
+    def subarray_sum_largest(x):
+        n = len(x)
+        temp_largest = x[0]
+        sum_largest = temp_largest
+        for num in x[1:]:
+            temp_largest = max(num, temp_largest + num)
+            sum_largest = max(sum_largest, temp_largest)
+            print(temp_largest)
+            print(sum_largest)
+        return sum_largest
+print(solution.subarray_sum_largest([-1, 4, -3, 4, -5]))
