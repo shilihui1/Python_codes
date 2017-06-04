84c15df08db9688e704fbb09eb053cb1c4f4369c
Lihui Shi <shilihui@uw.edu>
2017-06-04 12:27:13 -0700

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -4,15 +4,3 @@ print(b)
 c = pd.read_csv('/Users/lihuishi/Desktop/python codes/dict.csv', index_col = 0)
 print(c)
 
-# Print out country column as Pandas Series
-print(c['country'])
-print(type(c['country']))
-# Print out country column as Pandas DataFrame
-print(c[['country']])
-print(type(c[['country']]))
-# Print out first 3 observations
-print(c[0:3])
-# Print out observation for China: loc is label based 
-print(c.loc[['CN']])
-# Print out observations for India and China
-print(c.loc[['IN', 'CN']])
