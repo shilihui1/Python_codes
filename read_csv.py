62c00ae1b3903958e8946396854455d19cc6f3ae
Lihui Shi <shilihui@uw.edu>
2017-06-04 11:28:19 -0700

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -3,3 +3,10 @@ b = pd.read_csv('/Users/lihuishi/Desktop/python codes/dict.csv')
 print(b)
 c = pd.read_csv('/Users/lihuishi/Desktop/python codes/dict.csv', index_col = 0)
 print(c)
+
+# Print out country column as Pandas Series
+print(c['country'])
+print(type(c['country']))
+# Print out country column as Pandas DataFrame
+print(c[['country']])
+print(type(c[['country']]))
