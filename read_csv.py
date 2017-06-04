7b91baaa26f3f6008669003e3cb21f6407d0dd52
Lihui Shi <shilihui@uw.edu>
2017-06-03 23:27:16 -0700

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -1,3 +1,5 @@
 import pandas as pd
 b = pd.read_csv('/Users/lihuishi/Desktop/python codes/dict.csv')
 print(b)
+c = pd.read_csv('/Users/lihuishi/Desktop/python codes/dict.csv', index_col = 0)
+print(c)
