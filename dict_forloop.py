ddb724edd328194ec01a55effd0c4be88d569bef
Lihui Shi <shilihui@uw.edu>
2017-06-07 23:09:27 -0700

    Create dict_forloop.py

--- dict_forloop.py
+++ dict_forloop.py
@@ -1,4 +1,13 @@
 '''for loop in dictionary, using dictionary method a.items()'''
 a = {1: 'a', 2: 'b', 3: 'c', 4: 'd'}
+# items method for dictionary
 for key, value in a.items():
     print(str(key) + "--" + str(value))
+
+# keys method for dictionary
+for key in a.keys():
+    print(key)
+
+# values method for dictionary
+for value in a.values():
+    print(value)
