70004e057b007099c34fecb21eb2fa47f3d4af7c
Lihui Shi <shilihui@uw.edu>
2017-06-13 12:15:37 -0700

    Create remove_multiple_list.py

--- remove_multiple_list.py
+++ remove_multiple_list.py
@@ -1,4 +1,4 @@
 a = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 index_remove = [1, 2]
-b = [v for i, v in enumerate(a) if i not in index_remove]
+b = [v for key, value in enumerate(a) if key not in index_remove]
 print(b)
