8a62e0f22753ac8bc24dd1406be1b46f98073901
Lihui Shi <shilihui@uw.edu>
2017-06-13 11:28:26 -0700

    Create list_enumerate.py

--- list_enumerate.py
+++ list_enumerate.py
@@ -1,10 +1,10 @@
 a = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h'}
-for index, value in enumerate(a):
-    print(index, value)
+for key, value in enumerate(a):
+    print(key, value)
 
 b = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-for index, value in enumerate(b):
-    print(index, value)
+for key, value in enumerate(b):
+    print(key, value)
 
-for index, value in enumerate(b, start = 5):
-    print(index, value)
+for key, value in enumerate(b, start = 5):
+    print(key, value)
