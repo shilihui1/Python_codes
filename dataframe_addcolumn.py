fb2386b51c2363e45f5d634c4c8b31f1ec1c8faa
Lihui Shi <shilihui@uw.edu>
2017-06-28 21:45:44 -0700

    Update dataframe_addcolumn.py

--- dataframe_addcolumn.py
+++ dataframe_addcolumn.py
@@ -3,7 +3,7 @@ dict = {
 "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "SPretoria"],
 "area": [8.516, 17.10, 3.286, 9.597, 1.221],
 "population": [200.4, 143.5, 1252, 1357, 52.98]}
-type(dict)
+print(type(dict))
 
 import pandas as pd
 a = pd.DataFrame(dict)
