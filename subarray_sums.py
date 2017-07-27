ada690895b286e864bca5d85dd92ac07f26c554a
Lihui Shi <shilihui@uw.edu>
2017-07-26 23:59:03 -0700

    Update subarray_sums.py

--- subarray_sums.py
+++ subarray_sums.py
@@ -6,7 +6,7 @@ class solution:
         for i in range(1, n):
             # move the starting point for length i
             for j in range(n - i + 1):
-                a = sum(x[j:j + i])
+                a = sum(x[j:j + i - 1])
                 sum_subarray.append(a)
         return sum_subarray
 print(solution.subarray_sums([-1, 4, -3, 4, -5])) 
