fcb6fd6cbd3d61e84ffc0d2783b0ebefe9c793cc
Lihui Shi <shilihui@uw.edu>
2017-08-03 17:57:46 -0700

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -10,6 +10,21 @@ def isprime(n):
     else:
         print(n, "is a prime number")
         return True
+
+# only need to check whether x can be divided by a number from 1 to x/2
+import math
+def isprime(n):
+    """ find out whether a given number n is a prime number or not"""
+    if n == 1:
+        print("1 is a special number")
+        return False
+    for x in range(2, math.ceil(n/2) + 1):
+        if n%x == 0:
+            print(n, "is not a prime number")
+            return False
+    else:
+            print(n, "is a prime number")
+            return True
         
 for i in range(1, 24):
     print(isprime(i))
