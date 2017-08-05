596c8baefcb9cf261796bf42ac2e68f387754489
Lihui Shi <shilihui@uw.edu>
2017-08-05 14:12:31 -0700

    Update single_numer.py

--- single_numer.py
+++ single_numer.py
@@ -2,6 +2,7 @@
 Given an array of integers, every element appears twice except for one. Find that single one.
 Note: Your algorithm should have a linear runtime complexity.
 '''
+# below is not linear runtime
 class Solution(object):
     def singleNumber(self, nums):
         """
@@ -16,4 +17,20 @@ class Solution(object):
             else:
                 num_exist.append(nums[i])
         return num_exist[0]
+print(Solution().singleNumber([1, 2, 5, 4, 2, 1, 4]))
+
+# bitwise exclusive or, O(N)
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        if not A:
+            return None
+        p = A[0]
+        for i in range(1, len(A)):
+            print('p is ' + str(p))
+            print('A[i] is ' + str(A[i]))
+            p = p ^ A[i]
+            print('p becomes ' + str(p))
+        return p
 print(Solution().singleNumber([1, 2, 5, 4, 2, 1, 4]))
